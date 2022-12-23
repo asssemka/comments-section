bd98e9c8cdd18aabe30fa79b6e2ef405609955d6
asssemka <assem.kassenova@narxoz.kz>
2022-12-23 11:36:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,156 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONdata from './data';
+import { useState, useEffect } from 'react';
+
+import Comment from './components/Comment';
+import NewComment from './components/NewComment';
+import DeleteModal from './components/DeleteModal';
+let currentId = 5; // ¯\_(ツ)_/¯
 
 function App() {
+
+  const [data, setData] = useState(JSONdata);
+  const [deleteComment, setDeleteComment] = useState(false);
+  
+  const addNewReply = (id, content) => {
+    if (!/\S/.test(content)) return;  // to avoid posting empty comments (only whitespaces)
+    let temp = data;
+    currentId += 1;
+    for (let comment of temp.comments) {
+        if (comment.id === id) {
+        comment.replies.push({
+          'id': currentId + 1,
+          'content': content,
+          'createdAt': 'Just now',
+          'score': 0,
+          'replyingTo': comment.user.username,
+          'user': {...data.currentUser}
+        });
+        break;
+      }
+      if (comment.replies.length > 0) {
+        for (let reply of comment.replies) {
+          if (reply.id === id) {
+            comment.replies.push({
+              'id': currentId + 1,
+              'content': content,
+              'createdAt': 'Just now',
+              'score': 0,
+              'replyingTo': reply.user.username,
+              'user': {...data.currentUser}
+            });
+            break;
+          }
+        }
+      }
+    }
+    setData({...temp}); 
+  }
+
+  useEffect(() => {
+    localStorage.getItem("comments") !== null
+      ? setData(JSON.parse(localStorage.getItem("comments")))
+      : setData(JSONdata);
+  }, []);
+
+  useEffect(() => {
+    if(data.length !== 0){
+    localStorage.setItem("comments", JSON.stringify(data));
+    }
+  }, [data]);
+
+  const updateScore = (id, action) => {
+    let temp = data;
+    for (let comment of temp.comments) {
+      if (comment.id === id){
+        action == 'upvote' ? comment.score += 1 : comment.score -= 1;
+        break;
+      }
+      if (comment.replies.length > 0) {
+        for (let reply of comment.replies) {
+          if (reply.id === id) {
+            action == 'upvote' ? reply.score += 1 : reply.score -= 1;
+            break;
+          }
+        }
+      }
+    }
+    setData({...temp});
+  }
+
+  const updateComment = (updatedContent, id) => {
+    let temp = data;
+    for (let comment of temp.comments) {
+      if (comment.id === id) {
+        comment.content = updatedContent;
+        break;
+      } 
+      if (comment.replies.length > 0) {
+        for (let reply of comment.replies) {
+          if (reply.id === id) {
+            reply.content = updatedContent;
+            break;
+          }
+        }
+      }
+    }
+    setData({...temp});
+  }
+
+  const addNewComment = (content) => {
+    if (!/\S/.test(content)) return;
+    let temp = data;
+    currentId += 1;
+    temp.comments.push({
+      'id': currentId + 1,
+      'content': content,
+      'createdAt': 'Just now',
+      'score': 0,
+      'user': {...data.currentUser},
+      'replies': []
+    });
+    setData({...temp});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    { deleteComment !== false &&
+      <DeleteModal
+      id={deleteComment}
+      setDeleteComment={setDeleteComment}
+      setData={setData}
+      data={data}
+      />
+    }
+    
+    <main className='comments-column'>
+      { data.comments.map((comment) => {
+        return (
+            <Comment
+              replyingTo=''
+              addNewReply={addNewReply}
+              updateComment={updateComment}
+              setDeleteComment={setDeleteComment}
+              updateScore={updateScore}
+              key={comment.id}  
+              currentUser={data.currentUser}
+              comment={comment.content}
+              image={comment.user.image.png}
+              username={comment.user.username}
+              timeSince={comment.createdAt}
+              score={comment.score}
+              replies={comment.replies}
+              id={comment.id}
+            />
+          )
+      })
+      }
+      <NewComment
+        addNewComment={addNewComment}
+        currentUser={data.currentUser}
+      />
+    </main>
+    </>
   );
 }
 
